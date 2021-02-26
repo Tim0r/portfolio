a393caea4f1331771fa6eb6dc1b2e52575916f02
Tim0r <prelord123@gmai.com>
2021-02-27 00:19:13 +0600

    Project Upload

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,9 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def summary(self):
+        return self.body[:100]
+
+    def format_date(self):
+        return self.pub_date.strftime('%b %e %Y')
